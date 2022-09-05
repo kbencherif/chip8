03fe5efad6bb3c15167f588d5c174da0ed4864b4
Kamel <kbencherif29@gmail.com>
2022-09-05 02:03:20 -0400

    :sparkles: Add rom file reading and usage.

--- src/emulator/mod.rs
+++ src/emulator/mod.rs
@@ -1,18 +1,24 @@
+use std::fs;
+
+const START_ADDRESS: i32 = 0x200;
+
 #[derive(Debug)]
 pub struct Chip8 {
-    opcode: u16,
+    registers: Vec<u16>,
     memory: Vec<u16>,
     i: u16,
     pc: u16,
     stack: Vec<u16>,
     sp: u8,
-    screen: Vec<u16>
+    screen: Vec<u16>,
+    opcode: u16,
 }
 
 impl Chip8 {
     pub fn new() -> Chip8 {
         Chip8 {
             opcode: 0,
+            registers: vec![0; 16],
             memory: vec![0; 4096],
             i: 0,
             pc: 0,
@@ -20,5 +26,9 @@ impl Chip8 {
             sp: 0,
             screen: vec![0; 2048],
         }
+    }
+
+    pub fn load_rom(&self, path: String) {
+        let content = fs::read_to_string(path).expect("Should have been able to read the file");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+use std::env;
 mod emulator;
 
+fn print_help() {
+    println!("Usage: cargo run rom")
+}
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        print_help();
+        return
+    }
     let chip8 = emulator::Chip8::new();
+
+    println!("{}", args.len());
+    chip8.load_rom(args[1].clone())
 }
